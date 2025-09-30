c59848a9e921bf3fb40bf3b4016a68694aed8da3
PLaSMaX <maximfedorko@gmail.com>
2025-09-30 03:10:34 +0300

    Update to new .net maui

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PlasmaFamilyTree")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PlasmaFamilyTree.Pages")]
